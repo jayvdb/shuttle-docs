a8ae26d5f4c0b21cdb5991717bbd19fba1b2b180
John Vandenberg <jayvdb@gmail.com>
2023-06-05 08:26:57 +0800

    Add docs

--- import-examples/src/main.rs
+++ import-examples/src/main.rs
@@ -3,11 +3,13 @@ use std::path::{Path, PathBuf};
 
 use slugify::slugify;
 
+/// Find directories containing a `Cargo.toml`.
 fn rust_packages(examples_dir: &Path) -> Vec<PathBuf> {
     let paths = glob::glob(examples_dir.join("**/Cargo.toml").to_str().unwrap())
         .expect("Unable to read directory");
     paths
         .map(|x| {
+            // Remove `/Cargo.toml`
             let mut y = x.unwrap();
             y.pop();
             PathBuf::from(y.strip_prefix(examples_dir).unwrap())
@@ -20,25 +22,25 @@ fn slugify_snippet_name(example_package_path: String) -> String {
     slugify!(&example_package_path) + ".mdx"
 }
 
+/// Convert the file contents into a markdown code block.
 fn generate_snippet(file_path: String, language: String, display_name: String) -> String {
     let contents = std::fs::read_to_string(file_path).unwrap();
     format!("```{} {}\n{}\n```\n", language, display_name, contents)
 }
 
+/// Write `contents` to `filename`
 fn write_snippet(filename: &PathBuf, contents: String) {
     let mut f = std::fs::File::create(filename).unwrap();
     f.write_all(contents.as_bytes()).unwrap();
 }
 
-fn create_snippets(
-    examples_dir: &Path,
-    example_package_path: PathBuf,
-    snippet_dir: &Path,
-) {
+/// Convert a Rust package directory into a set of snippets
+fn create_snippets(examples_dir: &Path, example_package_path: PathBuf, snippet_dir: &Path) {
     let mut example_dir = PathBuf::from(examples_dir);
     example_dir.push(example_package_path.clone());
     let files =
         glob::glob(example_dir.join("**/*").to_str().unwrap()).expect("Unable to read directory");
+    // Ignore directories and .ignore files
     let files: Vec<PathBuf> = files
         .filter(|x| !x.as_ref().unwrap().metadata().unwrap().is_dir())
         .filter(|x| {
@@ -52,14 +54,10 @@ fn create_snippets(
         let relative_file_path =
             format!("{}/{}", example_package_path.display(), file_path.display());
         let snippet_name = slugify_snippet_name(relative_file_path.clone());
+        // Some filenames are foo.ext.example, and .example needs to be stripped
         let language_detect_file_name = match file_path.extension().unwrap() == "example" {
-            true => {
-                let x = file_path.clone();
-                PathBuf::from(x.file_stem().unwrap())
-            }
-            false => {
-                file_path.clone()
-            }
+            true => PathBuf::from(file_path.clone().file_stem().unwrap()),
+            false => file_path.clone(),
         };
         let language = detect_lang::from_path(language_detect_file_name)
             .unwrap_or_else(|| panic!("language detection of {:?} failed", file_path.clone()))
